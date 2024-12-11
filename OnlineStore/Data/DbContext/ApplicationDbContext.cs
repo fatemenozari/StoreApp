30c41e38934c69fcea54ac9b888c35897f6dc71a
F.Nozari <f.nozari@alibaba.ir>
2024-12-11 12:13:34 +0330

    Added DbContext and configuration files for Product and Category entities

---
+++ OnlineStore/Data/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using OnlineStore.Core.Domain;
+using OnlineStore.Data.Configuration;
+
+namespace OnlineStore.Data.DbContext
+{
+    public class ApplicationDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<Category> Categories { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
